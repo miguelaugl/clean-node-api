882faeec9becce9f17d75f24b24cfc32cc5d22c5
Miguel <miguelaugl@outlook.com>
2021-09-09 19:41:35 -0300

    chore: add eslint config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+    '.+\\.ts$': 'ts-jest',
+  },
 }
