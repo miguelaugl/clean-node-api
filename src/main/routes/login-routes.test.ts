5af292fd7144d7a60a9e916004a5cf2623d19ba2
Miguel <miguelaugl@outlook.com>
2021-09-12 18:09:44 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'miguelaugl@outlook.com',
+          password: '123',
+        })
+        .expect(401)
+    })
   })
 })
