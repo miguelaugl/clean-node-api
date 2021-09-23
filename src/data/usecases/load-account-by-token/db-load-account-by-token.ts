05dc1983007b1dd42578f4057c091be794bfb95c
Miguel <miguelaugl@outlook.com>
2021-09-23 13:18:32 -0300

    feat: ensure DbLoadAccountByToken calls Decrypter with correct values

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,14 @@
+import { Decrypter } from '@/data/protocols/cryptography/decrypter'
+import { AccountModel } from '@/domain/models/account'
+import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (
+    private readonly decrypter: Decrypter,
+  ) {}
+
+  async load (accessToken: string, role?: string): Promise<AccountModel> {
+    await this.decrypter.decrypt(accessToken)
+    return null
+  }
+}
