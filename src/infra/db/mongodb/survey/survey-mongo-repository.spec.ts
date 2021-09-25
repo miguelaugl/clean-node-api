e9012664acf3b01ee68aa3b9b74dfb26518dd370
Miguel <miguelaugl@outlook.com>
2021-09-25 20:35:51 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,10 +1,29 @@
 import { Collection } from 'mongodb'
 
+import { SurveyModel } from '@/domain/models/survey'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 import { SurveyMongoRepository } from './survey-mongo-repository'
 
 let surveyCollection: Collection
+
+const makeFakeSurveys = (): SurveyModel[] => ([{
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer',
+  }],
+  date: new Date(),
+}, {
+  id: 'other_id',
+  question: 'other_question',
+  answers: [{
+    image: 'other_image',
+    answer: 'other_answer',
+  }],
+  date: new Date(),
+}])
 
 const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository()
@@ -24,19 +43,32 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  it('should add survey on success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer',
-      }, {
-        answer: 'other_answer',
-      }],
-      date: new Date(),
+  describe('add()', () => {
+    it('should add survey on success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer',
+        }, {
+          answer: 'other_answer',
+        }],
+        date: new Date(),
+      })
+      const survey = surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  })
+
+  describe('loadAll', () => {
+    it('should load all surveys on success', async () => {
+      await surveyCollection.insertMany(makeFakeSurveys())
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
   })
 })
