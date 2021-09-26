2bbb77dffc1fd9d190d0b73ede037897245a4bba
Miguel <miguelaugl@outlook.com>
2021-09-25 21:36:57 -0300

    refactor: ensure imports use modules

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,8 +1,8 @@
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbAddSurvey } from '@/main/factories/usecases/add-survey/db-add-survey-factory'
 import { AddSurveyController } from '@/presentation/controllers/survey/add-survey/add-survey-controller'
 import { Controller } from '@/presentation/protocols'
 
-import { makeDbAddSurvey } from '../../../usecases/add-survey/db-add-survey-factory'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
 
 export const makeAddSurveyController = (): Controller => {
